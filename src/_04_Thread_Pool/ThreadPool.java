61957f0e18ad7cca1e60cc2d49493a3da6050774
League Student <league@iMac-25.attlocal.net>
2019-09-09 19:59:35 -0700

    thred but more

--- src/_04_Thread_Pool/ThreadPool.java
+++ src/_04_Thread_Pool/ThreadPool.java
@@ -1,5 +1,17 @@
 package _04_Thread_Pool;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class ThreadPool {
 
+	Thread[] threads;
+	ConcurrentLinkedQueue<Task> taskQueue;
+	
+	public ThreadPool(int totalThreads) {
+		threads = new Thread[totalThreads];
+		for (int i = 0; i < threads.length; i++) {
+			threads[i]
+		}
+	}
+	
 }
